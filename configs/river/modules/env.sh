b30e70ab962f545a201c4c1c58df5189ddbea767
Ming <mingmillennium@gmail.com>
2025-08-09 22:01:16 +0800

    Update config

--- configs/river/modules/env.sh
+++ configs/river/modules/env.sh
@@ -1,7 +1,8 @@
 # Config systemd and dbus env variables
 # It's necessary for xdg-desktop-portal, without it, xdg-desktop-portal won't work
 dbus-update-activation-environment --systemd \
-    XDG_CURRENT_DESKTOP \
-    WAYLAND_DISPLAY
+  DISPLAY \
+  XDG_CURRENT_DESKTOP \
+  WAYLAND_DISPLAY
 
 
