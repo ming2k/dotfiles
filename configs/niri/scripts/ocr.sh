dfd407b1c68edf9bce0a486238045cfe5d890a86
Ming <mingmillennium@gmail.com>
2026-01-01 22:45:55 +0800

    Adapt niri ocr command for void linux and optimize mpv config

--- configs/niri/scripts/ocr.sh
+++ configs/niri/scripts/ocr.sh
@@ -9,7 +9,9 @@ sleep 0.1
 grim -g "$(slurp)" "$TEMP_FILE"
 
 # Perform OCR and copy to clipboard
-tesseract "$TEMP_FILE" stdout -l chi_sim+chi_tra+eng+jpn --psm 6 --oem 3 2>/dev/null | wl-copy
+# tesseract "$TEMP_FILE" stdout -l chi_sim+chi_tra+eng+jpn --psm 6 --oem 3 2>/dev/null | wl-copy
+# For Void Linux
+tesseract-ocr "$TEMP_FILE" stdout -l eng --psm 6 --oem 3 2>/dev/null | wl-copy
 
 # Clean up
 rm -f "$TEMP_FILE"
